1255e1fde0025cac41fce3810d591824d7718faa
faridshere <130164710+faridshere@users.noreply.github.com>
2023-10-15 02:24:47 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,103 @@
-/* script.js */
+/* Reset some default styles */
+* {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+}
 
-const navLinks = document.querySelectorAll('nav a');
+/* Global styles */
+body {
+    font-family: Arial, sans-serif;
+    background-color: #f0f0f0;
+    line-height: 1.6;
+}
 
-navLinks.forEach(link => {
-  link.addEventListener
+header {
+    background-color: #333;
+    color: #fff;
+    padding: 20px;
+    text-align: center;
+}
+
+nav ul {
+    list-style: none;
+}
+
+nav ul li {
+    display: inline;
+    margin-right: 20px;
+}
+
+nav a {
+    text-decoration: none;
+    color: #fff;
+    transition: color 0.3s;
+}
+
+nav a:hover {
+    color: #ff4500;
+}
+
+/* Home section styles */
+#home {
+    text-align: center;
+    padding: 100px 0;
+}
+
+#home h1 {
+    font-size: 36px;
+    margin-bottom: 20px;
+}
+
+/* About section styles */
+#about {
+    background-color: #fff;
+    padding: 40px;
+}
+
+#about h2 {
+    font-size: 24px;
+    margin-bottom: 20px;
+}
+
+/* Services section styles */
+#services ul {
+    list-style: disc;
+    padding-left: 20px;
+}
+
+/* Portfolio section styles */
+.portfolio-item {
+    margin-bottom: 20px;
+}
+
+.portfolio-item img {
+    max-width: 100%;
+}
+
+/* Contact section styles */
+#contact form {
+    max-width: 400px;
+    margin: 0 auto;
+}
+
+#contact label,
+#contact input,
+#contact textarea {
+    display: block;
+    width: 100%;
+    margin-bottom: 10px;
+}
+
+#contact button {
+    background-color: #ff4500;
+    color: #fff;
+    border: none;
+    padding: 10px 20px;
+    cursor: pointer;
+}
+
+#contact button:hover {
+    background-color: #ff8c00;
+}
+
